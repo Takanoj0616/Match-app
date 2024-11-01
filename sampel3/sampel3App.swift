216f20b6d17281fa01552afd3b55e5b675922e9d
Takanoj0616 <60534220+Takanoj0616@users.noreply.github.com>
2024-11-01 13:57:03 +0900

    Initial Commit

---
+++ sampel3/sampel3App.swift
@@ -0,0 +1,17 @@
+//
+//  sampel3App.swift
+//  sampel3
+//
+//  Created by 高野純一 on 2024/11/01.
+//
+
+import SwiftUI
+
+@main
+struct sampel3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
